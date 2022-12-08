52f1032abf2c33f049840103665b5d8ca9a79f66
Atroopa <atroopa@gmail.com>
2022-12-08 11:35:37 +0330

    change image to text palce

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@
 
 function App() {
   return (
-    <div className="min-h-screen w-screen bg-gradient-to-r from-[#F0F4FD] via-purple-200 to-[#A1A3BA]" >
+    <div className="flex flex-col-reverse justify-center items-center min-h-screen w-screen bg-gradient-to-r from-[#F0F4FD] via-purple-200 to-[#A1A3BA]" >
+
+
+
+      <div className="mt-5">
+        <h1 className="font-black text-4xl text-center">
+          Fresh Healty
+          Delicious Salads
+        </h1>
+
+        <p>
+        we made fresh and healty foods
+        </p>
+
+        <span>
+          Explore
+        </span>
+      </div>
+
+
+
       <img src={process.env.PUBLIC_URL + "/assets/food.svg"} 
            alt="food" 
            className="w-screen" />
-      
-      <h1>
-        Fresh Healty
-        Delicious Salads
-        we made fresh and healty foods
-      </h1>
+
 
     </div>
   );
