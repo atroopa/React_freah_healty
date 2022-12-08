32e0715bb9823566d086cfd7e5a2303b980dee2c
Atroopa <atroopa@gmail.com>
2022-12-08 12:05:07 +0330

    set animate true

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState, useEffect } from "react";
 
 
 function App() {
+
+  const [isAnimated, setIsAnimated] = useState(false); 
+
+  useEffect(() => {
+    setIsAnimated(true);
+  }, []);
+
+
   return (
-    <div className="lg:flex-row flex flex-col-reverse justify-center items-center min-h-screen w-screen bg-gradient-to-r from-[#F0F4FD] via-purple-200 to-[#A1A3BA]" >
+    <div className="lg:justify-around lg:flex-row flex flex-col-reverse justify-center items-center min-h-screen w-screen bg-gradient-to-r from-[#F0F4FD] via-purple-200 to-[#A1A3BA]" >
 
-
-
-      <div className="mt-5 text-center">
-        <h1 className="font-black text-5xl">
+      <div className="mt-5 text-center lg:text-left">
+        <h1 className="font-black text-5xl lg:text-6xl">
           Fresh Healty <br/>
           <span className="font-mono font-light">Delicious Salads</span>
         </h1>
@@ -24,8 +31,9 @@ function App() {
 
 
       <img src={process.env.PUBLIC_URL + "/assets/food.svg"} 
-           alt="food" 
-           className="w-screen lg:max-w-lg" />
+           alt="food"
+           className={`${isAnimated ? 'scale-150': ''
+                        } w-screen lg:max-w-lg transform transition duration-1000 ease-in-out`} />
 
 
     </div>
